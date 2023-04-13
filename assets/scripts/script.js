3184f32e9d0078f58774e23a78b3657e69f2e7e4
Jen B <jenbutler984@gmail.com>
2023-04-13 09:27:59 -0400

    Made some changes to the way game over is displayed, minor edits here/there

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,49 +8,79 @@ const scoreBackButton = document.querySelector("#score-back-btn");
 const scoreClearButton = document.querySelector("#score-clr-btn");
 
 // The question text with options, warning message about correct/incorrect questions, the timer and scorekeeper
+const questionOptionArea = document.querySelector("#question-area");
 const questionOptionText = document.querySelector(".question-text");
-const questionOptionButton = document.querySelector(".question-choices");
 const rightWrongMessage = document.querySelector(".right-wrong-msg");
 const timerElement = document.querySelector(".timer-count");
 const displayGameScore = document.querySelector(".display-score");
-const qnForm = document.querySelector(".qn-form");
 
 // Field to record username, the final score table, the stored high scores, max score table size
-const scoreUsername = document.querySelector(".score-username");
+const saveScoreButton = document.querySelector("#save-score-button");
+const gameOverSection = document.querySelector("#game-over-section");
+const finalScoreElement = document.querySelector("#final-score");
 const finalScoreTable = document.querySelector(".final-score-table");
 const finalScoreList = JSON.parse(localStorage.getItem("finalScoreList")) || []; // from local storage or empty array
-const maxScoreTableSize = 3;
+const maxScoreTableSize = 10;
 
 // Setting working items that will be increasing or decreasing during the game
 let timer;
-let timerCount = 60;
+let timerCount = 120;
 let questionNo = 0;
 let gameScore = 0;
-let worstScore = 0;
+let questionContainer;
 
 // The questions, choices, and answers
 const questionSheet = [
   {
-    question: "Which of the following is not a data type in JavaScript?",
-    choices: ["Selector", "Object", "Symbol", "Undefined"],
-    answer: "Selector",
+    question: "What is the correct syntax to declare a variable in JavaScript?",
+    choices: ["var myVar = 10;", "variable myVar = 10;", "int myVar = 10;", "declare myVar = 10;"],
+    answer: "var myVar = 10;",
   },
   {
-    question: "Which of the following values is truthy?",
-    choices: ["x = 0", "NaN", "y = 'Bootcamp'", "null === undefined"],
-    answer: "y = 'Bootcamp'",
+    question: "Which of the following is not a valid JavaScript comparison operator?",
+    choices: ["===", "==", "=!=", "!=="],
+    answer: "=!=",
   },
   {
-    question: "Which of the following values is not truthy?",
-    choices: ["0 == false", "1 == '1'", "0 === false", "null == undefined"],
-    answer: "0 === false",
+    question: "What does the 'typeof' operator return?",
+    choices: ["The type of a variable", "The length of a variable", "The value of a variable", "The index of a variable"],
+    answer: "The type of a variable",
   },
   {
-    question:
-      "Which of the following is not one of the three states of promise?",
-    choices: ["Pending", "Fulfilled", "Rejected", "Pinky"],
-    answer: "Pinky",
+    question: "How do you create a function in JavaScript?",
+    choices: ["function myFunction(){}", "func myFunction(){}", "function: myFunction(){}", "createFunction myFunction(){}"],
+    answer: "function myFunction(){}",
   },
+  {
+    question: "Which method can be used to add a new element to the end of an array?",
+    choices: ["append()", "add()", "push()", "attach()"],
+    answer: "push()"
+  },
+  {
+    question: "What is the correct way to create an object in JavaScript?",
+    choices: ["let obj = (key: 'value');", "let obj = [key: 'value'];", "let obj = {key: 'value'};", "let obj = /key: 'value';/"],
+    answer: "let obj = {key: 'value'};"
+  },
+  {
+    question: "How do you create a comment in JavaScript that spans multiple lines?",
+    choices: ["/* Comment /", "// Comment", "# Comment", "<!-- Comment -->"],
+    answer: "/ Comment */"
+  },
+  {
+    question: "How do you access the first element of an array named 'myArray'?",
+    choices: ["myArray.0", "myArray(0)", "myArray[1]", "myArray[0]"],
+    answer: "myArray[0]"
+  },
+  {
+    question: "Which of these is not a valid way to create a new date object in JavaScript?",
+    choices: ["new Date()", "new Date(2023, 3, 10)", "Date(2023, 3, 10)", "new Date('April 10, 2023')"],
+    answer: "Date(2023, 3, 10)"
+  },
+  {
+    question: "How do you convert a string to a number in JavaScript?",
+    choices: ["Number(string)", "parseInt(string)", "parseFloat(string)", "All of the above"],
+    answer: "All of the above"
+  }  
 ];
 
 // Assign multiple attributes to an element
@@ -60,21 +90,28 @@ function setAttributes(el, attrs) {
   }
 }
 
+// start quiz button listener
 if (startQuizButton) {
   startQuizButton.addEventListener("click", function (event) {
-    var element = event.target;
-    startQuizButton.disabled = true;
-    showQuestion(questionNo);
-    startTimer();
+    event.preventDefault();
+    startQuizButton.disabled = true; // disables the start button during the game
+    showQuestion(questionNo); // starts the questions
+    startTimer(); // starts the timer
   });
 }
 
+// builds out questions by creating elements, probably not the most efficient way to do it seemed a pretty flexible way to go about it
 function showQuestion(i) {
   questionOptionText.innerHTML = "";
 
   var currentQuestion = questionSheet[i];
-  var questionContainer = document.createElement("form");
+  questionContainer = document.createElement("form");
 
+  questionContainer.addEventListener("submit", function (event) {
+    event.preventDefault();
+    submitAnswer();
+  });
+ 
   questionContainer.textContent = currentQuestion.question;
   questionContainer.setAttribute("class", "qn-form");
 
@@ -124,89 +161,99 @@ function startTimer() {
 }
 
 if (submitButton) {
-  submitButton.addEventListener("click", function () {
+  submitButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    submitAnswer();
+  });
+}
 
-    if (timerCount <= 0) {
-      rightWrongMessage.textContent = "Game over!";
-      return;
+function submitAnswer() {
+  currentQuestion = questionSheet[questionNo];
+  questionChoices = currentQuestion.choices;
+
+  if (timerCount <= 0) {
+    rightWrongMessage.textContent = "Try again by hitting Start Quiz!";
+    questionContainer.textContent = "The timer is at 0. Game over!";
+    displayGameScore.textContent = gameScore;
+    startQuizButton.disabled = false;
+    return;
+  }
+
+  var questionAnswer = currentQuestion.answer;
+  var selectedAnswer = null;
+
+  for (var i = 0; i < questionChoices.length; i++) {
+    var optionsButton = document.getElementById(questionChoices[i]);
+    if (optionsButton.checked) {
+      selectedAnswer = optionsButton.id;
+      break;
     }
+  }
 
-    currentQuestion = questionSheet[questionNo];
-    questionChoices = currentQuestion.choices;
+  if (selectedAnswer === null) {
+    rightWrongMessage.textContent = "Error! No answer is selected. Please select an answer!";
+    return;
+  }
 
-    var questionAnswer = currentQuestion.answer;
-    var selectedAnswer = null;
+  if (selectedAnswer === questionAnswer) {
+    rightWrongMessage.textContent = "You are correct!";
+    gameScore++;
+    displayGameScore.textContent = gameScore;
+  } else {
+    rightWrongMessage.textContent = "Incorrect! You've lost time! Next question.";
+    timerCount -= 10;
+  }
 
-    for (var i = 0; i < questionChoices.length; i++) {
-      var optionsButton = document.getElementById(questionChoices[i]);
-      if (optionsButton.checked) {
-        selectedAnswer = optionsButton.id;
-        break;
+  questionNo++;
+
+  if (questionNo < questionSheet.length) {
+    showQuestion(questionNo);
+  } else {
+    rightWrongMessage.textContent = "Game over!";
+    displayGameScore.textContent = gameScore;
+    startQuizButton.disabled = false;
+    checkScore();
+  }
+}
+
+function checkScore() {
+  questionOptionArea.style.display = "none";
+  gameOverSection.style.display = "block";
+  finalScoreElement.textContent = gameScore;
+}
+
+if (saveScoreButton) {
+  saveScoreButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    const usernameInput = document.getElementById("username-input");
+    const username = usernameInput.value;
+
+    if (username) {
+      finalScoreList.push({ gameScore, namePrompt: username });
+      finalScoreList.sort((a, b) => (a.gameScore > b.gameScore ? -1 : 1));
+
+      if (finalScoreList.length > maxScoreTableSize) {
+        finalScoreList.pop();
       }
-    }
 
-    if (selectedAnswer === null) {
-      console.log("No answer is selected.");
-      return;
-    }
-
-    if (selectedAnswer === questionAnswer) {
-      console.log("you are correct");
-      rightWrongMessage.textContent = "You are correct!";
-      gameScore++;
-      displayGameScore.textContent = gameScore;
+      localStorage.setItem("finalScoreList", JSON.stringify(finalScoreList));
+      window.location.href = "highscore.html";
     } else {
-      console.log("you are incorrect");
-      rightWrongMessage.textContent = "You are wrong!";
-      timerCount -= 10;
-    }
-
-    questionNo++;
-
-    if (questionNo < questionSheet.length) {
-      showQuestion(questionNo);
-    } else {
-      console.log("Quiz complete.");
-      rightWrongMessage.textContent = "Game over!";
-      displayGameScore.textContent = gameScore;
-      startQuizButton.disabled = false;
-      checkScore();
+      alert("Please enter your name.");
     }
   });
 }
 
-function checkScore() {
-  if (finalScoreList.length > maxScoreTableSize) {
-    worstScore = finalScoreList[finalScoreList.length - 1].gameScore;
-  }
-
-  if (gameScore > worstScore) {
-    const namePrompt = window.prompt(
-      `${gameScore}! Top score! What's your name?`
-    );
-    finalScoreList.push({ gameScore, namePrompt });
-  } else {
-    alert("Game over!\nSorry, you didn't get a high score.\nTry again!");
-  }
-
-  finalScoreList.sort((a, b) => (a.gameScore > b.gameScore ? -1 : 1));
-
-  if (finalScoreList.length > maxScoreTableSize) {
-    finalScoreList.pop();
-  }
-  localStorage.setItem("finalScoreList", JSON.stringify(finalScoreList));
-}
-
 if (viewHighScoreButton) {
   viewHighScoreButton.addEventListener("click", function (event) {
-    var element = event.target;
+    event.preventDefault();
     window.location.href = "highscore.html";
   });
 }
 
 if (scoreBackButton) {
   scoreBackButton.addEventListener("click", function (event) {
-    var element = event.target;
+    event.preventDefault();
     console.log("clicking score back button");
     window.location.href = "index.html";
   });
@@ -214,6 +261,7 @@ if (scoreBackButton) {
 
 if (scoreClearButton) {
   scoreClearButton.addEventListener("click", function (event) {
+    event.preventDefault();
     finalScoreList.splice(0, finalScoreList.length);
     localStorage.setItem("finalScoreList", JSON.stringify(finalScoreList));
     displayScore(finalScoreList, finalScoreTable);
@@ -227,7 +275,7 @@ function displayScore() {
   const headerUser = document.createElement("th");
   const headerScore = document.createElement("th");
 
-  headerUser.textContent = "User";
+  headerUser.textContent = "Name";
   headerScore.textContent = "Score";
 
   headerRow.appendChild(headerUser);
